06b35a6fd6378a698a043d55fa517439adae7649
manuelmag1 <maglioni124@gmail.com>
2026-03-20 22:25:59 +0100

    Restore all organized images from previous backup (img/discover/, img/gastronomy/, etc.)

---
+++ docs/js/scripts.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#136dec",
+                "background-light": "#f6f7f8",
+                "background-dark": "#101822",
+            },
+            fontFamily: {
+                "display": ["Inter", "sans-serif"]
+            },
+            borderRadius: { "DEFAULT": "0.25rem", "lg": "0.5rem", "xl": "0.75rem", "full": "9999px" },
+        },
+    },
+};
